f9e5ace55d26ea855e91cc60ed175d2d7c784f1d
Rirei Kuroshima <kuroshima@d-meister.co.jp>
2015-11-09 11:11:43 +0900

    golang: 紗凪を叩くサンプルプログラムを作成する
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

---
+++ golang/sanaClient.go
@@ -0,0 +1,40 @@
+//
+// Copyright (c) 2015 Rirei Kuroshima
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const baseurl = "http://api.moemoe.tokyo/anime/v1/twitter/follwer/"
+
+	var accounts string
+	flag.StringVar(&accounts, "a", "", "取得したいTwitterアカウント")
+	flag.Parse()
+
+	if accounts == "" {
+		return
+	}
+
+	url := baseurl + "status?accounts=" + accounts
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := string(body)
+
+	fmt.Println(result)
+}
