70fd7169c52a2b897520cb234d00fe2d8e58fdc9
Rirei Kuroshima <rireikuroshima@icloud.com>
2015-11-10 20:12:43 +0900

    golang: Add GetFollowerHistory()-function
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

--- golang/sana/getTwitter.go
+++ golang/sana/getTwitter.go
@@ -47,3 +47,42 @@ Usage:
 
 	return string(body), nil
 }
+
+// GetFollowerHistory()関数は、引数で受け取った（アニメに関する）Twitter
+// アカウントのフォロワー数の履歴をSana（紗凪）
+// <https://github.com/Project-ShangriLa/sana_server>から取得します。
+// 第1引数が対象のTwitterアカウント、第2引数に日時を指定します。
+// この引数はunixtimestampで指定します。`
+func GetFollowerHistory(args ...string) (string, error) {
+	const usageString = `
+Usage:
+    func GetFollowerHistory(args ...string) (string, error)
+    引数で取得したツイッターアカウントのフォロワー数の履歴を取得します。
+    第1引数が対象のTwitterアカウント、第2引数に日時を指定します。
+    この引数はunixtimestampで指定します。`
+
+	url :=  baseurl + "history"
+
+	if len(args) == 1 {
+		url += "?accounts=" + args[0]
+	} else if len(args) == 2 {
+		url += "?accounts=" + args[0] +
+			"&end_date=" + args[1]
+	} else {
+		const msg = "空のスライスが渡されています。\n"
+		return "", errors.New(msg + usageString)
+	}
+
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
