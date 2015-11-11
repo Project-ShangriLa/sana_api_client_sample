b00f8a2c891be0a339e973b0aeb23d64ca8bfe91
Rirei Kuroshima <rireikuroshima@icloud.com>
2015-11-11 23:00:11 +0900

    golang: testing: Add TestChangeTime() function
    YYYY-MM-DD HH:ii:ss形式の文字列を受け取ってそれをunix時間に変換
    する関数のテストコードを追加。
    
    Signed-off-by: Rirei Kuroshima <rireikuroshima@icloud.com>

--- golang/sana/getTwitter_test.go
+++ golang/sana/getTwitter_test.go
@@ -33,3 +33,47 @@ func TestGetJson(t *testing.T) {
 		t.Errorf("Expected %v, but %v ", expected, actual)
 	}
 }
+
+var testdata = []struct {
+	in string
+	out string
+	e string
+}{
+	{"2015-11-05 15:04:05", "1446703445", ""},
+	{"2015-11-05_15:04:05", "", "YYYY-MM-DD HH:ii:ssで指定してください。"},
+	{"201511-05 15:04:05", "", "年月日はYYYY-MM-DDで指定してください。"},
+	{"2015-1105 15:04:05", "", "年月日はYYYY-MM-DDで指定してください。"},
+	{"20151105 15:04:05", "", "年月日はYYYY-MM-DDで指定してください。"},
+	{"2015-11-05-06 15:04:05", "", "年月日はYYYY-MM-DDで指定してください。"},
+	{"2016-11-05 15:04:05", "", "年月日を正しく指定してください。"},
+	{"2015-13-05 15:04:05", "", "年月日を正しく指定してください。"},
+	{"2015-00-05 15:04:05", "", "年月日を正しく指定してください。"},
+	{"2015-11-32 15:04:05", "", "年月日を正しく指定してください。"},
+	{"2015-11-00 15:04:05", "", "年月日を正しく指定してください。"},
+	{"2015-11-05 1504:05", "", "時分秒はHH:ii:ssで指定してください。"},
+	{"2015-11-05 15:0405", "", "時分秒はHH:ii:ssで指定してください。"},
+	{"2015-11-05 150405", "", "時分秒はHH:ii:ssで指定してください。"},
+	{"2015-11-05 24:04:05", "", "時分秒を正しく指定してください。"},
+	{"2015-11-05 15:60:05", "", "時分秒を正しく指定してください。"},
+	{"2015-11-05 15:04:60", "", "時分秒を正しく指定してください。"},
+}
+
+func TestChangeTime(t *testing.T) {
+	for _, tt := range testdata {
+		actual, actualErr := changeTime(tt.in)
+
+		if tt.out != "" {
+			expected := tt.out
+			if actual != expected {
+				t.Errorf("Expected: %v, but %v: ", expected,
+					actual)
+			}
+		} else {
+			expectedErr := errors.New(tt.e)
+			if actualErr.Error() != expectedErr.Error() {
+				t.Errorf("\n\tExpected: %v\n\tActual: %v",
+					expectedErr, actualErr)
+			}
+		}
+	}
+}
